adbe4cc5932459048cab91bd64a30dcf1abda692
Sobhandev Pramanik <sobhandevp2021@gmail.com>
2024-01-10 00:41:41 +0530

    add filter  && fixe some responsive nes

--- client/src/component/Home.jsx
+++ client/src/component/Home.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import '../css/Home.css';
 import photo1 from '../photo/photo1.png';
 import ShopeList from './ShopeList';
 import photo2 from '../photo/photo2.png';
+import demoPhoto from '../photo/images.jpg';
 function Home() {
+  const shopes=[
+    {
+      name:"Chai point",
+      photo:demoPhoto,
+      rating:4,
+      foods:"Bakery, Beverages, Maharashtrian,",
+      offer:"150 for two",
+      delevery_time:"25 Minutes"
+    },
+    {
+      name:"Chai point",
+      photo:demoPhoto,
+      rating:3.5,
+      foods:"Bakery, Beverages, Maharashtrian,",
+      offer:"500 for five",
+      delevery_time:"30 Minutes"
+    },
+    {
+      name:"Afc",
+      photo:demoPhoto,
+      rating:4.4,
+      foods:"Bakery, Beverages, Maharashtrian,",
+      offer:"200 for two",
+      delevery_time:"40 Minutes"
+    },
+    {
+      name:"Food zone",
+      photo:demoPhoto,
+      rating:3.3,
+      foods:"Bakery, Beverages, Maharashtrian,",
+      offer:"250 for three",
+      delevery_time:"33 Minutes"
+    },
+    {
+      name:"Pizza hat",
+      photo:demoPhoto,
+      rating:2.3,
+      foods:"Bakery, Beverages, Maharashtrian,",
+      offer:"350 for four",
+      delevery_time:"23 Minutes"
+    }
+  ]
+  // For search a shope
+const [filterShope,setFilterShope]=useState(shopes);
+const filterByName=(e)=>{
+//  console.log(e.target.value);
+  setFilterShope(shopes.filter((shope)=>{
+    return shope.name.toLowerCase().includes(e.target.value)
+ }));
+}
+const filterByRating=(rating)=>{
+  // console.log(e.target.value);
+   setFilterShope(shopes.filter((shope)=>{
+     return shope.rating>=rating;
+  }));
+ }
+
   return (
     <div className='home'>
      <div className='home-above-div'>
@@ -11,12 +69,27 @@ function Home() {
         
             <img  src={photo1} alt='photo1' />
      </div>
-     <div className='shope-list-div'>
-<ShopeList/>
-<ShopeList/>
-<ShopeList/>
-<ShopeList/>
-<ShopeList/>
+     <div className='shope-midile-div'>
+      <div className='search-div'> 
+      <div className='search-rating-div' >
+        <button className='rating-search-btn' onClick={()=>{filterByRating(-1)}}>All Rated</button>
+        <button className='rating-search-btn' onClick={()=>{filterByRating(2)}}>2+ Rated</button>
+        <button className='rating-search-btn' onClick={()=>{filterByRating(3)}}>3+ Rated</button>
+        <button className='rating-search-btn' onClick={()=>{filterByRating(4)}}>4+ Rated</button>
+      </div>
+        <input type="search" id='search-input' onChange={filterByName} name="search" placeholder='Type for search' />
+      </div>
+      <div className='shope-list-div'>
+      {
+        filterShope.length>0 ?filterShope.map((shope)=>{
+
+          return <ShopeList shope={shope}/>
+        }):<h2>No shope found</h2>
+      }
+      </div>
+     
+
+
 </div>
 <div className='home-bottom-img'>
   <img src={photo2} alt='photo2'/>
--- client/src/component/ShopeList.jsx
+++ client/src/component/ShopeList.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import '../css/ShopeList.css';
-import demoPhoto from '../photo/images.jpg';
+// import demoPhoto from '../photo/images.jpg';
 import { useNavigate } from 'react-router-dom';
 import StarOutlinedIcon from '@mui/icons-material/StarOutlined';
-function ShopeList() {
+function ShopeList(props) {
+  const {name,photo,rating,  foods,offer, delevery_time}=props.shope;
     const navigate=useNavigate();
     // for demo
-    const food="Bakery, Beverages, Maharashtrian, Snacks, Street Food, South Indian, Punjabi, Chaat, Indian, American, North Indian, Fast Food, Desserts, Cafe, Healthy Food, Home Food"
+    // const food="Bakery, Beverages, Maharashtrian, Snacks, Street Food, South Indian, Punjabi, Chaat, Indian, American, North Indian, Fast Food, Desserts, Cafe, Healthy Food, Home Food"
   return (
     <div className='shope-list' onClick={()=>{navigate(`/shope/shope-id`)}}>
-        <img className='shope-photo overflow-hidden' src={demoPhoto} alt='shope-phot'/>
-        <h2 className='shope-name overflow-hidden'>Chai Point</h2>
+        <img className='shope-photo overflow-hidden' src={photo} alt='shope-phot'/>
+        <h2 className='shope-name overflow-hidden'>{name}</h2>
         <div className='ratting-div overflow-hidden'>
-            <h3>4.8</h3>
+            <h3>{rating}</h3>
             <StarOutlinedIcon className='ratting-icon'/>
         </div>
-        <p className='food overflow-hidden'>{food.slice(0,35)}...</p>
-        <p className='offer overflow-hidden'>₹150 for two</p>
+        <p className='food overflow-hidden'>{foods.slice(0,35)}...</p>
+        <p className='offer overflow-hidden'>{offer}</p>
         <h3 className='delivery-time overflow-hidden'>
-        43 Minutes
+       {delevery_time}
         </h3>
 
     </div>
