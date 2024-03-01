18545920e5bb09d591409ece7eb61c874a273e7f
Sobhandev Pramanik <sobhandevp2021@gmail.com>
2024-03-01 19:23:29 +0530

    Delet somthing for local machine problem

--- server/httpError.js
+++
@@ -1,9 +0,0 @@
-exports.httperrors={
-    Bad_Request:400,
-    InvalidFormat: 403 ,
-    Not_Found:404 ,
-    Unauthorized: 401,
-    Conflict:409,
-    Invalid_input:422,
-   Server_Error: 500  
-}
