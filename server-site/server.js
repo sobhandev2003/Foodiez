44d4b15e63c3df75303c62c35f856011c2088d1c
sobhandev2003 <sobhandevp2021@gmail.com>
2025-04-15 11:07:37 +0530

    change cors

--- server-site/server.js
+++ server-site/server.js
@@ -17,7 +17,7 @@ app.use(cors(
     {
         origin: [
             'http://localhost:3000',
-            'https://sobhan-foodiez.netlify.app/',
+            'https://sobhan-foodiez.netlify.app',
         ],
        
     }
